1e7003186c59f25c2475d67a3e6edf37286d32a3
eldrid01 <eldrid01@server.fake>
2014-09-05 17:12:44 +0100

    No longer creates specialisations for each dataset as these no longer in use, added configuration of bowtie and exonerate executable paths, option to specify generation of separate reports for each dataset, fixed configuration of output directory for LSF log files, added check for existence of files specified in sample sheet.

--- src/main/java/org/cruk/mga/workflow/CreateMetadataFromSampleSheet.java
+++ src/main/java/org/cruk/mga/workflow/CreateMetadataFromSampleSheet.java
@@ -24,13 +24,11 @@
 package org.cruk.mga.workflow;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -39,10 +37,11 @@ import org.apache.commons.cli.ParseException;
 import org.cruk.util.CommandLineUtility;
 import org.cruk.workflow.assembly.MetaDataLoader;
 import org.cruk.workflow.util.ApplicationContextFactory;
+import org.cruk.workflow.util.FileFinder;
 import org.cruk.workflow.xml2.metadata.MetaData;
 import org.cruk.workflow.xml2.metadata.ModeConfiguration;
-import org.cruk.workflow.xml2.metadata.Specialisation;
-import org.cruk.workflow.xml2.metadata.SpecialisationSet;
+import org.cruk.workflow.xml2.pipeline.FilenamePattern;
+import org.cruk.workflow.xml2.pipeline.TaskVariableSet;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -57,18 +56,24 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
     private static final String DEFAULT_RESOURCES_DIR = "${install}/resources";
     private static final String DEFAULT_DATA_DIR = "${work}";
     private static final String DEFAULT_OUTPUT_DIR = "${work}";
+    private static final String DEFAULT_BOWTIE_EXECUTABLE = "bowtie";
+    private static final String DEFAULT_EXONERATE_EXECUTABLE = "exonerate";
+    private static final boolean DEFAULT_SEPARATE_DATASET_REPORTS = false;
     private static final String DEFAULT_LSF_QUEUE = "bioinformatics";
 
-    String sampleSheetFilename;
-    String workingDirectory;
-    String temporaryDirectory;
-    String resourcesDirectory;
-    String dataDirectory;
-    String outputDirectory;
+    private FileFinder fileFinder = new FileFinder();
+
+    private String sampleSheetFilename;
+    private String workingDirectory;
+    private String temporaryDirectory;
+    private String resourcesDirectory;
+    private String dataDirectory;
+    private String outputDirectory;
+    private String bowtieExecutable;
+    private String exonerateExecutable;
+    private boolean separateDatasetReports;
 
     private String runId;
-    private List<String> datasetIds = new ArrayList<String>();
-    private Map<String, String> filenameMappings = new HashMap<String, String>();
 
     private MetaData meta;
     private ModeConfiguration lsf;
@@ -102,12 +107,18 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
         options.addOption("r", "resources-directory", true, "The MGA resources directory containing the bowtie-indexes subdirectory, the adaptor sequences and the reference genome mapping (default: " + DEFAULT_RESOURCES_DIR + ")");
         options.addOption(null, "data-directory", true, "The data directory containing the FASTQ sequence files (default: " + DEFAULT_DATA_DIR + ")");
         options.addOption(null, "output-directory", true, "The output directory in which the report is created (default: " + DEFAULT_OUTPUT_DIR + ")");
+        options.addOption("b", "bowtie-executable", true, "The path for the bowtie executable (default: " + DEFAULT_BOWTIE_EXECUTABLE + ")");
+        options.addOption("e", "exonerate-executable", true, "The path for the exonerate executable (default: " + DEFAULT_EXONERATE_EXECUTABLE + ")");
+        options.addOption(null, "separate-dataset-reports", false, "If separate reports for each dataset are required");
 
         workingDirectory = DEFAULT_WORKING_DIR;
         temporaryDirectory = DEFAULT_TEMP_DIR;
         resourcesDirectory = DEFAULT_RESOURCES_DIR;
         dataDirectory = DEFAULT_DATA_DIR;
         outputDirectory = DEFAULT_OUTPUT_DIR;
+        bowtieExecutable = DEFAULT_BOWTIE_EXECUTABLE;
+        exonerateExecutable = DEFAULT_EXONERATE_EXECUTABLE;
+        separateDatasetReports = DEFAULT_SEPARATE_DATASET_REPORTS;
 
         try
         {
@@ -143,6 +154,21 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
                 outputDirectory = commandLine.getOptionValue("output-directory");
             }
 
+            if (commandLine.hasOption("bowtie-executable"))
+            {
+            	bowtieExecutable = commandLine.getOptionValue("bowtie-executable");
+            }
+
+            if (commandLine.hasOption("exonerate-executable"))
+            {
+            	exonerateExecutable = commandLine.getOptionValue("exonerate-executable");
+            }
+
+            if (commandLine.hasOption("separate-dataset-reports"))
+            {
+            	separateDatasetReports = commandLine.hasOption("separate-dataset-reports");
+            }
+
             args = commandLine.getArgs();
 
             if (args.length < 1)
@@ -162,18 +188,12 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
     {
         readSampleSheet();
 
-        if (runId == null)
-        {
-            logger.error("Run ID must be specified");
-            System.exit(1);
-        }
-
         meta.setMode("local");
         meta.setTempDirectory(temporaryDirectory);
         meta.setPipeline("${install}/pipelines/mga.xml");
 
         lsf = new ModeConfiguration("lsf");
-        lsf.setVar("lsfOutputDirectory", "${work}/lsfOutput");
+        lsf.setVar("outputDirectory", "${work}/lsfOutput");
         lsf.setVar("queue", DEFAULT_LSF_QUEUE);
         lsf.setVar("maximumSubmittedJobs", "50");
         meta.getModeConfigurations().add(lsf);
@@ -195,23 +215,11 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
         meta.setVariable("bowtieIndexDir", "${resourcesDir}/bowtie_indexes");
         meta.setVariable("adapterFastaFile", "${resourcesDir}/adapters.fa");
         meta.setVariable("referenceGenomeMappingFile", "${resourcesDir}/reference_genome_mappings.txt");
-        meta.setVariable("bowtieExecutable", "bowtie");
-        meta.setVariable("exonerateExecutable", "exonerate");
-        meta.setVariable("separateDatasetReports", "true");
+        meta.setVariable("bowtieExecutable", bowtieExecutable);
+        meta.setVariable("exonerateExecutable", exonerateExecutable);
+        meta.setVariable("separateDatasetReports", Boolean.toString(separateDatasetReports));
         meta.setVariable("plotWidth", "800");
         meta.setVariable("minimumSequenceCount", "10");
-
-        meta.getSpecialisationSets().add(new SpecialisationSet());
-        SpecialisationSet specialisationSet = meta.getSpecialisationSet(0);
-        for (String dataset : datasetIds)
-        {
-            Specialisation specialisation = new Specialisation(dataset);
-            specialisation.setActive(true);
-            String filename = filenameMappings.get(dataset);
-            if (!filename.startsWith("./") && !filename.startsWith("/")) filename = "${dataDir}/" + filename;
-            specialisation.setVariable("fastqFile", filename);
-            specialisationSet.add(specialisation);
-        }
     }
 
     /**
@@ -248,38 +256,72 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
 
         try
         {
-            boolean inDatasetSection = false;
-            String line = null;
-            while ((line = reader.readLine()) != null)
+        	boolean inDatasetSection = false;
+
+            int lineNumber = 0;
+        	String line = null;
+
+        	while ((line = reader.readLine()) != null)
             {
+                lineNumber++;
                 String[] fields = line.split("\\t");
                 if (inDatasetSection)
                 {
-                    if (fields.length < 2) break;
-                    String id = fields[0].trim();
-                    String filename = fields[1].trim();
-                    if (id.length() == 0 || filename.length() == 0) break;
-                    if (!filenameMappings.containsKey(id))
+                    if (fields.length < 2)
                     {
-                        datasetIds.add(id);
-                        filenameMappings.put(id, filename);
+                    	error("Dataset section in sample sheet contains line with less than two columns at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
+                    }
+
+                    String id = fields[0].trim();
+                    if (id.length() == 0)
+                    {
+                    	error("Missing dataset identifier in first column at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
+                    }
+
+                    String filename = fields[1].trim();
+                    if (filename.length() != 0)
+                    {
+                    	if (!dataDirectory.equals(DEFAULT_DATA_DIR))
+                    	{
+                    		filename = filename.replaceAll("\\$\\{dataDir\\}", dataDirectory);
+                    	}
+                    	String[] filenames = filename.split("\\|");
+                    	for (int i = 0; i < filenames.length; i++)
+                    	{
+                    		File file = new File(filenames[i]);
+                    		List<File> files = fileFinder.findFiles(file.getAbsolutePath(), FilenamePattern.WILDCARD, TaskVariableSet.INPUT);
+                    		if (files.isEmpty())
+                    		{
+                    			logger.warn("Could not find any files matching pattern " + fields[1].trim() + " at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
+                    		}
+                    	}
                     }
                 }
                 else
                 {
                     if (fields.length > 1)
                     {
-                        if (fields[0].trim().equals("DatasetId") && fields[1].trim().equals("File"))
+                        if (fields[0].trim().equalsIgnoreCase("DatasetId") && fields[1].trim().equalsIgnoreCase("File"))
                         {
                             inDatasetSection = true;
                         }
-                        else if (runId == null && fields[0].equals("Run ID") && fields[1].trim().length() > 0)
+                        else if (runId == null && fields[0].equalsIgnoreCase("Run ID") && fields[1].trim().length() > 0)
                         {
                             runId = fields[1];
                         }
                     }
                 }
             }
+
+        	if (runId == null)
+        	{
+        		error("Run ID must be specified in header section in sample sheet file " + sampleSheetFilename);
+        	}
+
+        	if (!inDatasetSection)
+        	{
+        		error("Missing dataset section in sample sheet file " + sampleSheetFilename);
+        	}
         }
         catch (IOException e)
         {
