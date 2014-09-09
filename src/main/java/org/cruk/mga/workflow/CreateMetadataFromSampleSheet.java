90feb3dce2d8e94a1059eae9c3776634de6822f5
eldrid01 <eldrid01@server.fake>
2014-09-09 22:21:09 +0100

    Added configuration options for the execution mode, maximum number of processors or submitted jobs, cluster queue, sample size, etc.

--- src/main/java/org/cruk/mga/workflow/CreateMetadataFromSampleSheet.java
+++ src/main/java/org/cruk/mga/workflow/CreateMetadataFromSampleSheet.java
@@ -51,18 +51,35 @@ import org.springframework.context.ApplicationContext;
  */
 public class CreateMetadataFromSampleSheet extends CommandLineUtility
 {
+	private static final String DEFAULT_MODE = "local";
+    private static final int DEFAULT_MAX_CPU_RESOURCES = 1;
+    private static final String DEFAULT_QUEUE = "bioinformatics";
+    private static final int DEFAULT_MAX_SUBMITTED_JOBS = 50;
+
     private static final String DEFAULT_WORKING_DIR = "@{user.dir}";
     private static final String DEFAULT_TEMP_DIR = "${work}/temp";
     private static final String DEFAULT_RESOURCES_DIR = "${install}/resources";
     private static final String DEFAULT_DATA_DIR = "${work}";
     private static final String DEFAULT_OUTPUT_DIR = "${work}";
+    
     private static final String DEFAULT_BOWTIE_EXECUTABLE = "bowtie";
     private static final String DEFAULT_EXONERATE_EXECUTABLE = "exonerate";
+
+    private static final int DEFAULT_SAMPLE_SIZE = 100000;
+    private static final int DEFAULT_MAX_RECORDS_TO_SAMPLE_FROM = 5000000;
+    private static final int DEFAULT_CHUNK_SIZE = 5000000;
+    private static final int DEFAULT_TRIM_LENGTH = 36;
+
+    private static final int DEFAULT_PLOT_WIDTH = 800;
+    private static final int DEFAULT_MIN_SEQUENCE_COUNT = 10;
     private static final boolean DEFAULT_SEPARATE_DATASET_REPORTS = false;
-    private static final String DEFAULT_LSF_QUEUE = "bioinformatics";
 
     private FileFinder fileFinder = new FileFinder();
 
+    private String mode;
+    private int maxCpuResources;
+    private String queue;
+    private int maxSubmittedJobs;
     private String sampleSheetFilename;
     private String workingDirectory;
     private String temporaryDirectory;
@@ -71,13 +88,19 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
     private String outputDirectory;
     private String bowtieExecutable;
     private String exonerateExecutable;
+    private int sampleSize;
+    private int maxNumberOfRecordsToSampleFrom;
+    private int chunkSize;
+    private int trimLength;
+    private int plotWidth;
+    private int minimumSequenceCount;
     private boolean separateDatasetReports;
 
     private String runId;
 
     private MetaData meta;
-    private ModeConfiguration lsf;
     private ModeConfiguration local;
+    private ModeConfiguration lsf;
 
     public static void main(String[] args)
     {
@@ -101,16 +124,30 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
     {
         CommandLineParser parser = new GnuParser();
 
-        options.addOption("o", "output-metadata-file", true, "Output pipeline metadata file (default: stdout)");
+        options.addOption("o", "output-metadata-file", true, "Output pipeline metadata (configuration) file (default: stdout)");
+        options.addOption("m", "mode", true, "The run mode, either local or lsf (default: " + DEFAULT_MODE + ")");
+        options.addOption("n", "max-cpu-resources", true, "Maximum number of CPU processors to use when running in local mode (default: " + DEFAULT_MAX_CPU_RESOURCES + ")");
+        options.addOption("q", "queue", true, "The queue to submit to when using the LSF scheduler (default: " + DEFAULT_QUEUE + ")");
+        options.addOption("j", "max-submitted-jobs", true, "Maximum number of submitted jobs when using the LSF scheduler (default: " + DEFAULT_MAX_SUBMITTED_JOBS + ")");
         options.addOption("w", "working-directory", true, "The working directory (default: current directory, referred to as ${work})");
         options.addOption("t", "temp-directory", true, "The temporary directory (default: " + DEFAULT_TEMP_DIR + ")");
-        options.addOption("r", "resources-directory", true, "The MGA resources directory containing the bowtie-indexes subdirectory, the adaptor sequences and the reference genome mapping (default: " + DEFAULT_RESOURCES_DIR + ")");
-        options.addOption(null, "data-directory", true, "The data directory containing the FASTQ sequence files (default: " + DEFAULT_DATA_DIR + ")");
+        options.addOption("r", "resources-directory", true, "The MGA resources directory containing the bowtie-indexes subdirectory, the adaptor sequences and the reference genome mapping (default: " + DEFAULT_RESOURCES_DIR + ", where ${install} refers to the MGA installation directory)");
+        options.addOption("d", "data-directory", true, "The data directory containing the FASTQ sequence files (default: " + DEFAULT_DATA_DIR + ")");
         options.addOption(null, "output-directory", true, "The output directory in which the report is created (default: " + DEFAULT_OUTPUT_DIR + ")");
         options.addOption("b", "bowtie-executable", true, "The path for the bowtie executable (default: " + DEFAULT_BOWTIE_EXECUTABLE + ")");
         options.addOption("e", "exonerate-executable", true, "The path for the exonerate executable (default: " + DEFAULT_EXONERATE_EXECUTABLE + ")");
+        options.addOption("s", "sample-size", true, "The number of FASTQ records to sample for each dataset (default: " + DEFAULT_SAMPLE_SIZE + ")");
+        options.addOption(null, "max-records-to-sample-from", true, "The maximum number of FASTQ records to read (sample from) for each dataset (default: " + DEFAULT_MAX_RECORDS_TO_SAMPLE_FROM + ")");
+        options.addOption("c", "chunk-size", true, "The maximum number of FASTQ records in each chunk/alignment job (default: " + DEFAULT_CHUNK_SIZE + ")");
+        options.addOption("l", "trim-length", true, "The length to trim sequences to for alignment (default: " + DEFAULT_TRIM_LENGTH + ")");
+        options.addOption(null, "plot-width", true, "The width of the stacked bar plot in pixels (default: " + DEFAULT_PLOT_WIDTH + ")");
+        options.addOption(null, "min-sequence-count", true, "The minimum sequence count to use on the y-axis when creating the stacked bar plot (default: " + DEFAULT_MIN_SEQUENCE_COUNT + ")");
         options.addOption(null, "separate-dataset-reports", false, "If separate reports for each dataset are required");
 
+        mode = DEFAULT_MODE;
+        maxCpuResources = DEFAULT_MAX_CPU_RESOURCES;
+        queue = DEFAULT_QUEUE;
+        maxSubmittedJobs = DEFAULT_MAX_SUBMITTED_JOBS;
         workingDirectory = DEFAULT_WORKING_DIR;
         temporaryDirectory = DEFAULT_TEMP_DIR;
         resourcesDirectory = DEFAULT_RESOURCES_DIR;
@@ -118,6 +155,12 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
         outputDirectory = DEFAULT_OUTPUT_DIR;
         bowtieExecutable = DEFAULT_BOWTIE_EXECUTABLE;
         exonerateExecutable = DEFAULT_EXONERATE_EXECUTABLE;
+        sampleSize = DEFAULT_SAMPLE_SIZE;
+        maxNumberOfRecordsToSampleFrom = DEFAULT_MAX_RECORDS_TO_SAMPLE_FROM;
+        chunkSize = DEFAULT_CHUNK_SIZE;
+        trimLength = DEFAULT_TRIM_LENGTH;
+        plotWidth = DEFAULT_PLOT_WIDTH;
+        minimumSequenceCount = DEFAULT_MIN_SEQUENCE_COUNT;
         separateDatasetReports = DEFAULT_SEPARATE_DATASET_REPORTS;
 
         try
@@ -127,6 +170,44 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
             if (commandLine.hasOption("output-metadata-file"))
             {
                 outputFilename = commandLine.getOptionValue("output-metadata-file");
+            }
+
+            if (commandLine.hasOption("mode"))
+            {
+                mode = commandLine.getOptionValue("mode").toLowerCase();
+                if (!mode.equals("local") && !mode.equals("lsf"))
+                {
+                	error("Error: unrecognized execution mode: " + commandLine.getOptionValue("mode") + " (can be either local or lsf)");
+                }
+            }
+
+            if (commandLine.hasOption("max-cpu-resources"))
+            {
+                try
+                {
+                	maxCpuResources = Integer.parseInt(commandLine.getOptionValue("max-cpu-resources"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: max-cpu-resources must be an integer number.");
+                }
+            }
+
+            if (commandLine.hasOption("queue"))
+            {
+                queue = commandLine.getOptionValue("queue");
+            }
+
+            if (commandLine.hasOption("max-submitted-jobs"))
+            {
+                try
+                {
+                	maxSubmittedJobs = Integer.parseInt(commandLine.getOptionValue("max-submitted-jobs"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: max-submitted-jobs must be an integer number.");
+                }
             }
 
             if (commandLine.hasOption("working-directory"))
@@ -164,6 +245,78 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
             	exonerateExecutable = commandLine.getOptionValue("exonerate-executable");
             }
 
+            if (commandLine.hasOption("sample-size"))
+            {
+                try
+                {
+                	sampleSize = Integer.parseInt(commandLine.getOptionValue("sample-size"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: sample-size must be an integer number.");
+                }
+            }
+
+            if (commandLine.hasOption("max-records-to-sample-from"))
+            {
+                try
+                {
+                	maxNumberOfRecordsToSampleFrom = Integer.parseInt(commandLine.getOptionValue("max-records-to-sample-from"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: max-records-to-sample-from must be an integer number.");
+                }
+            }
+
+            if (commandLine.hasOption("chunk-size"))
+            {
+                try
+                {
+                	chunkSize = Integer.parseInt(commandLine.getOptionValue("chunk-size"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: chunk-size must be an integer number.");
+                }
+            }
+
+            if (commandLine.hasOption("trim-length"))
+            {
+                try
+                {
+                	trimLength = Integer.parseInt(commandLine.getOptionValue("trim-length"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: trim-length must be an integer number.");
+                }
+            }
+
+            if (commandLine.hasOption("plot-width"))
+            {
+                try
+                {
+                	plotWidth = Integer.parseInt(commandLine.getOptionValue("plot-width"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: plot-width must be an integer number.");
+                }
+            }
+
+            if (commandLine.hasOption("min-sequence-count"))
+            {
+                try
+                {
+                	minimumSequenceCount = Integer.parseInt(commandLine.getOptionValue("min-sequence-count"));
+                }
+                catch (NumberFormatException e)
+                {
+                    error("Error parsing command line option: min-sequence-count must be an integer number.");
+                }
+            }
+
             if (commandLine.hasOption("separate-dataset-reports"))
             {
             	separateDatasetReports = commandLine.hasOption("separate-dataset-reports");
@@ -188,19 +341,19 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
     {
         readSampleSheet();
 
-        meta.setMode("local");
+        meta.setMode(mode);
         meta.setTempDirectory(temporaryDirectory);
         meta.setPipeline("${install}/pipelines/mga.xml");
 
         local = new ModeConfiguration("local");
         local.setVar("outputDirectory", "${work}/logs");
-        local.setVar("maxCpuResources", "1");
+        local.setVar("maxCpuResources", Integer.toString(maxCpuResources));
         meta.getModeConfigurations().add(local);
 
         lsf = new ModeConfiguration("lsf");
         lsf.setVar("outputDirectory", "${work}/logs");
-        lsf.setVar("queue", DEFAULT_LSF_QUEUE);
-        lsf.setVar("maximumSubmittedJobs", "50");
+        lsf.setVar("queue", queue);
+        lsf.setVar("maximumSubmittedJobs", Integer.toString(maxSubmittedJobs));
         meta.getModeConfigurations().add(lsf);
 
         meta.setVariable("runId", runId);
@@ -214,13 +367,13 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
         meta.setVariable("referenceGenomeMappingFile", "${resourcesDir}/reference_genome_mappings.txt");
         meta.setVariable("bowtieExecutable", bowtieExecutable);
         meta.setVariable("exonerateExecutable", exonerateExecutable);
-        meta.setVariable("sampleSize", "100000");
-        meta.setVariable("maxNumberOfRecordsToSampleFrom", "5000000");
-        meta.setVariable("chunkSize", "5000000");
-        meta.setVariable("trimLength", "36");
+        meta.setVariable("sampleSize", Integer.toString(sampleSize));
+        meta.setVariable("maxNumberOfRecordsToSampleFrom", Integer.toString(maxNumberOfRecordsToSampleFrom));
+        meta.setVariable("chunkSize", Integer.toString(chunkSize));
+        meta.setVariable("trimLength", Integer.toString(trimLength));
+        meta.setVariable("plotWidth", Integer.toString(plotWidth));
+        meta.setVariable("minimumSequenceCount", Integer.toString(minimumSequenceCount));
         meta.setVariable("separateDatasetReports", Boolean.toString(separateDatasetReports));
-        meta.setVariable("plotWidth", "800");
-        meta.setVariable("minimumSequenceCount", "10");
     }
 
     /**
@@ -270,13 +423,13 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
                 {
                     if (fields.length < 2)
                     {
-                    	error("Dataset section in sample sheet contains line with less than two columns at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
+                    	error("Error: dataset section in sample sheet contains line with less than two columns at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
                     }
 
                     String id = fields[0].trim();
                     if (id.length() == 0)
                     {
-                    	error("Missing dataset identifier in first column at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
+                    	error("Error: missing dataset identifier in first column at line " + lineNumber + " of sample sheet file " + sampleSheetFilename);
                     }
 
                     String filename = fields[1].trim();
@@ -316,12 +469,12 @@ public class CreateMetadataFromSampleSheet extends CommandLineUtility
 
         	if (runId == null)
         	{
-        		error("Run ID must be specified in header section in sample sheet file " + sampleSheetFilename);
+        		error("Error: Run ID must be specified in header section in sample sheet file " + sampleSheetFilename);
         	}
 
         	if (!inDatasetSection)
         	{
-        		error("Missing dataset section in sample sheet file " + sampleSheetFilename);
+        		error("Error: missing dataset section in sample sheet file " + sampleSheetFilename);
         	}
         }
         catch (IOException e)
