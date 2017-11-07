95501c9dbaedfa8197bc2b0d7cbc932bd2871789
eldrid01 <matthew.d.eldridge@gmail.com>
2017-11-07 17:33:34 +0000

    Updated date in copyright notice.

--- src/main/java/org/cruk/util/LineCounter.java
+++ src/main/java/org/cruk/util/LineCounter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2013 Cancer Research UK Cambridge Institute
+ * Copyright (c) 2017 Cancer Research UK Cambridge Institute
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
